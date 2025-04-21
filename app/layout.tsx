b7df2a4c8fd7d25ce5076e9592712e54490e1e64
jeaneselinasm <jeanessmanuhutu@gmail.com>
2025-04-21 14:04:35 +0900

    update:app/layout.tsc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,5 @@
 // app/layout.tsx
-import './globals.css'
-// import Script from 'next/script'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
-export const metadata: Metadata = {
-  title: 'Bahtraku',
-  description: 'Supporting Bible translation projects worldwide',
-}
 
-export default function RootLayout({
-  children,
-  params
-}: {
-  children: React.ReactNode,
-  params: { locale: string };
-}) {
-  return (
-    <html lang={params.locale || 'en'  }>
-      <head />
-      <body className={inter.className}>{children}
-      </body>
-    </html>
-  )
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return children;
 }
