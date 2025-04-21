88d477952a4523be90e0d89aa27021f69daf5ccb
jeaneselinasm <jeanessmanuhutu@gmail.com>
2025-04-21 14:50:09 +0900

    update:tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss"
 const shadcnConfig = require("./node_modules/ui/tailwind.config.js")
 
 const config = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
